bcd88c504be2e5bc2a3249b1221582760ff89b10
Nithishvikky <nithishudhayakumar007@gmail.com>
2025-04-09 23:08:37 +0530

    Task2 completed

---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Person{
+    private string name;
+    private int age;
+    public Person(){}
+    public Person(string userName,int userAge){
+        name = userName;
+        age = userAge;
+    }
+    // Propeties, It's like getter setter but it's built-in keywords
+    public string Name{
+        get{return name;}
+        set{name = value;}
+    }
+    public int Age{
+        get{return age;}
+        set{age = value;}
+    }
+    
+    //Public method we can use it in all classes
+    public void Introduce(){
+        Console.WriteLine($"Welcome to the C# Class {age} year old {Name}");
+    }
+}
+class Program{
+    static void Main(){
+        Person p1 = new Person("Nithish",20);
+        Person p2 = new Person();
+        Console.Write("Enter a person name : ");
+        p2.Name = Console.ReadLine();
+        Console.Write("Enter a person age : ");
+        p2.Age = Convert.ToInt32(Console.ReadLine());
+        p1.Introduce();
+        p2.Introduce();
+    }
+}
---
+++ Task2/README.md
@@ -0,0 +1,12 @@
+# Simple Object-Oriented Programming 
+
+## Notes
+- If you don't write any constructor, Compiler automatically create default one.
+- If you write parameterized constructor , You have to manually write default construtor.
+- It will affect while initiating object for class.
+
+## Learned things
+- Properties and get,set built-in keywords
+
+## Output
+![output](output.png)

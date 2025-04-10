3d44e3c039fa4c1a3c73f474120e8da0472b2257
Nithishvikky <nithishudhayakumar007@gmail.com>
2025-04-10 18:49:26 +0530

    Task4 completed

---
+++ Task4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+class Program{
+    static void Main(){
+        List<Student> students = new List<Student>{
+            new Student { Name = "Aarav", Age = 16, Grade = 90 },
+            new Student { Name = "Aarav", Age = 15, Grade = 95 },
+            new Student { Name = "Meera", Age = 17, Grade = 85 },
+            new Student { Name = "Karan", Age = 15, Grade = 78 },
+            new Student { Name = "Sneha", Age = 16, Grade = 92 },
+            new Student { Name = "Rohit", Age = 18, Grade = 88 },
+            new Student { Name = "Divya", Age = 17, Grade = 91 },
+            new Student { Name = "Yash", Age = 16, Grade = 74 },
+            new Student { Name = "Anjali", Age = 15, Grade = 96 },
+            new Student { Name = "Vikram", Age = 18, Grade = 69 },
+            new Student { Name = "Pooja", Age = 17, Grade = 83 }
+        };
+
+        List<Student>filteredList = students.Where(s => s.Grade > 80)
+                                    .OrderBy(s => s.Name).ThenBy(s => s.Age).ToList();
+                                    
+        Console.WriteLine("Student List");
+        foreach(Student s in students){
+            Console.WriteLine($"Name : {s.Name} Age : {s.Age} Grade : {s.Grade}");
+        }
+        Console.WriteLine();
+        Console.WriteLine("Filtered Student List");
+        foreach(Student s in filteredList){
+            Console.WriteLine($"Name : {s.Name} Age : {s.Age} Grade : {s.Grade}");
+        }
+    }
+}
+
+public class Student{
+    public string Name{ get; set; }
+    public int Age{ get; set; }
+    public int Grade{ get; set; }
+}
---
+++ Task4/README.md
@@ -0,0 +1,19 @@
+# Working with Collections and LINQ
+
+## What I have done here
+ - First I created a class for student which contains the name,age and grade properties.
+ - I used list to store 10 students data which list of Students `List<Student> students`.
+ - And used LINQ methods to filter(`Where`) and sort(`OrderBy`,`ThenBy`) data which is stored in another list.
+ - By using `ToList()`, I converted the return data from this`IOrderedEnumerable<Student>` to `List<Student>`.
+ 
+
+## Learned Things
+- LINQ which is used to save and retrive data from collection like SQL.
+- Two types of syntax 
+ -- Method syntax
+    --- Used this in the task. We can methods to filter and sort data.
+ -- Query syntax
+    --- It's like SQL query. Have write query to filter and sort data.
+
+## Output
+![Output](output.png)
